1a71363d9505352d55bc1eec59dc24241d380265
Ganu <Ganu@GANU-PC>
2021-07-18 22:09:26 +0530

    certificate

--- PDFCertificate/src/pdf/certificate/Certificate.java
+++ PDFCertificate/src/pdf/certificate/Certificate.java
@@ -30,7 +30,7 @@ public class Certificate {
 
 	}
 	
-	public String getCertificate(String name,String discription,String qrData,String signDate) throws IOException{	
+	public String getCertificate(String name,String discription,String qrData,String signDate) throws Exception{	
 		
 		//Document and Page Creation
 		PDDocument doc = new PDDocument();
@@ -50,7 +50,7 @@ public class Certificate {
 		PDImageXObject sign = PDImageXObject.createFromFile("src\\template\\signature.png", doc);
 		content.drawImage(sign, 660, 170, 160, 50);
 		
-		//Color Objs Creation
+		//Color Object Creation
 		Color nameColor = new Color(253, 47, 111);
 		Color blackColor = Color.BLACK;
 		
@@ -75,6 +75,8 @@ public class Certificate {
 		content.showText(name);		
 		content.endText();	
 		
+		//Load Sign Date on the certificate 
+		
 		//Calculate the Sign Date String Height and Width for Center the Text		
 		int signDateFontSize = 14; // Or whatever font size you want.
 		float signDateWidth = latoFont.getStringWidth(signDate) / 1000 * signDateFontSize; // get your text width 
@@ -91,48 +93,43 @@ public class Certificate {
 		content.endText();		
 		
 		//QR-Code Generation	
-		QRCode qr = new QRCode();
-		try {
-			BufferedImage QRBufferedImage = qr.generateQR(qrData);
-			PDImageXObject QRImage = LosslessFactory.createFromImage(doc, QRBufferedImage);
-			content.drawImage(QRImage, 85, 80);					
-		}catch(Exception ex) {
-			ex.printStackTrace();
-		}		
+		QRCode qr = new QRCode();		
+		BufferedImage QRBufferedImage = qr.generateQR(qrData);
+		PDImageXObject QRImage = LosslessFactory.createFromImage(doc, QRBufferedImage);
+		content.drawImage(QRImage, 85, 80);					
+				
 	    
 		//Certificate Content 
-		try {
+		
 			//Content Parameters 
-			float contentFontSize = 12;
-			float margin = 200;
-			PDRectangle mediabox = page.getMediaBox(); 
-			float width = mediabox.getWidth() - 2*margin; 
-			float leading = 1.5f * contentFontSize;
-			//passing the content parameters to paragraph method in the Text class. 
-			//That is return the lines as a Arraylist. 
-			Text text = new Text(page);		
-			ArrayList<String> lines = text.paragraph(discription, latoFont, contentFontSize, width);
-		    content.beginText();
-		    content.setFont(latoFont, contentFontSize);	 
-		    float contentWidth = latoFont.getStringWidth(lines.get(0)) / 1000 * contentFontSize; // get your text width 
-			float contentHeight = latoFont.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * contentFontSize; // get your text height
-			content.newLineAtOffset((page.getMediaBox().getWidth() - contentWidth) / 2 , page.getMediaBox().getHeight() - 280 - contentHeight); 
-		  //  content.newLineAtOffset(280, 280);
+		float contentFontSize = 12;
+		float margin = 200;
+		PDRectangle mediabox = page.getMediaBox(); 
+		float width = mediabox.getWidth() - 2*margin; 
+		float leading = 1.5f * contentFontSize;
+		//passing the content parameters to paragraph method in the Text class. 
+		//That is return the lines as a Arraylist. 
+		Text text = new Text(page);		
+		ArrayList<String> lines = text.paragraph(discription, latoFont, contentFontSize, width);
+		content.beginText();
+		content.setFont(latoFont, contentFontSize);	 
+		float contentWidth = latoFont.getStringWidth(lines.get(0)) / 1000 * contentFontSize; // get your text width 
+		float contentHeight = latoFont.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * contentFontSize; // get your text height
+		content.newLineAtOffset((page.getMediaBox().getWidth() - contentWidth) / 2 , page.getMediaBox().getHeight() - 280 - contentHeight); 
+		 //  content.newLineAtOffset(280, 280);
 			
-			//Printing the array list with text align center logic.
-			int i = 0;		
-			for (String line: lines){
-		    	if(i>=0) {
-		    		float contentWidth2 = latoFont.getStringWidth(line) / 1000 * contentFontSize; // get your text width
-			    	content.newLineAtOffset((width - contentWidth2) / 2 , -leading);
-		    	}	        
-		    	content.showText(line);	 
-		    	i++;
-		    }
-		    content.endText(); 
-		}catch(Exception ex) {
-			ex.printStackTrace();
-		}		
+		//Printing the array list with text align center logic.
+		int i = 0;		
+		for (String line: lines){
+		    if(i>=0) {
+		    	float contentWidth2 = latoFont.getStringWidth(line) / 1000 * contentFontSize; // get your text width
+			    content.newLineAtOffset((width - contentWidth2) / 2 , -leading);
+		    }	        
+		    content.showText(line);	 
+		    i++;
+		}
+		content.endText(); 
+			
 	   		
 		content.close();//Should close the Content Stream in final stage.		
 		doc.save("C:\\Users\\Ganu\\Desktop\\Final\\"+qrData+"-HIT-Certificate.pdf");//Save the Document Path as where you want.
